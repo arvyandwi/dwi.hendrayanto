38abc18013a67df7283753d99c46598d42156d9b
Arvyan Dwi <ArvyanDwi19@gmail.com>
2025-04-18 14:28:54 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "dwi.hendrayanto",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "QA Engineer",
     bio: "I develop everything using node.",
     email: "arvyandwi19@gmail.com",
